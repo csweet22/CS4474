3a345d0dca9e1e825039ad717496f09b61064cd0
Isaac Bercovitz Green <isaac@gritz.ca>
2025-03-22 14:34:14 -0400

    Added delay to minigame completion

--- Assets/Scripts/Minigames/Minigame.cs
+++ Assets/Scripts/Minigames/Minigame.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 
 public abstract class Minigame : MonoBehaviour
 {
     [SerializeField] private int xp;
+    private float delay = 0.5f;
     
     public void CompleteMinigame(bool isCorrect)
     {
+        StartCoroutine(CompleteWithDelay(isCorrect));
+    }
+
+    private IEnumerator CompleteWithDelay(bool isCorrect)
+    {
+        yield return new WaitForSeconds(delay);
+        
         if (isCorrect)
         {
             Managers.ProgressionManager.Instance.AddXp(xp);
