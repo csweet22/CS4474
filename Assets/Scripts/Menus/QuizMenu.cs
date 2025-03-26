9ccff07ba7eb12101569c0604869a9f91e0797dd
Isaac Bercovitz Green <isaac@gritz.ca>
2025-03-26 18:34:35 -0400

    Add hint button

--- Assets/Scripts/Menus/QuizMenu.cs
+++ Assets/Scripts/Menus/QuizMenu.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class QuizMenu : ACMenu
 {
     [SerializeField] private Button backButton;
+    [SerializeField] private Button hintButton;
+    [SerializeField] private GameObject hintText;
 
     public override void Open()
     {
@@ -16,15 +16,22 @@ public class QuizMenu : ACMenu
     private void OnEnable()
     {
         backButton.onClick.AddListener(OnBackClicked);
+        hintButton.onClick.AddListener(OnHintClicked);
     }
 
     private void OnBackClicked()
     {
         Managers.QuizManager.Instance.EndQuiz();
     }
+    
+    private void OnHintClicked()
+    {
+        hintText.SetActive(!hintText.activeSelf);
+    }
 
     private void OnDisable()
     {
         backButton.onClick.RemoveAllListeners();
+        hintButton.onClick.RemoveAllListeners();
     }
 }
